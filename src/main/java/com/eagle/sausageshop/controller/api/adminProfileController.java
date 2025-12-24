b6fabaac9ce2e05e8e01b68748d2b75b42856119
lakshitha <lakshithapiuma09@gmail.com>
2025-12-24 15:44:26 +0530

    SausageShop My Viva Project

--- src/main/java/com/eagle/sausageshop/controller/api/adminProfileController.java
+++ src/main/java/com/eagle/sausageshop/controller/api/adminProfileController.java
@@ -1,19 +1,34 @@
 package com.eagle.sausageshop.controller.api;
 
+import com.eagle.sausageshop.annotation.IsUser;
 import com.eagle.sausageshop.dto.UserDTO;
 import com.eagle.sausageshop.service.AdminService;
 import com.eagle.sausageshop.util.AppUtil;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.servlet.http.HttpSession;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
 @Path("/admin")
-public class adminController {
+public class adminProfileController {
+
+    @IsUser
+    @Path("/logout")
+    @GET
+    public Response logout(@Context HttpServletRequest request) {
+        HttpSession httpSession = request.getSession(false);
+        if (httpSession != null && httpSession.getAttribute("user") != null) {
+            httpSession.invalidate();
+            return Response.status(Response.Status.OK).build();
+        } else {
+            System.out.println("else");
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+    }
+
+
     @Path("/admin-login")
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
---
+++ src/main/webapp/assets/js/adminPofile.js
@@ -0,0 +1,53 @@
+window.addEventListener("load", async () => {
+    Notiflix.Loading.pulse("Data is loading", {
+        clickToClose: false,
+        svgColor: '#0284c7'
+    });
+
+    try {
+
+    } finally {
+        Notiflix.Loading.remove();
+    }
+});
+
+
+
+
+
+async function logOut(){
+    // alert("ok");
+    Notiflix.Loading.pulse("Wait...", {
+        clickToClose: false,
+        svgColor: '#0284c7'
+    });
+
+    try {
+        const response = await fetch("api/admin/logout", {
+            method: "GET",
+            credentials: "include"
+        });
+        if (response.ok) {
+
+            Notiflix.Report.success(
+                'SausageShop Admin Panel',
+                "Logout successful",
+                'Okay',
+                () => {
+                    window.location = "admin-login.html";
+                }
+            );
+        } else {
+            Notiflix.Notify.failure("Something went wrong. Logout failed!", {
+                position: 'center-top'
+            });
+        }
+
+    } catch (e) {
+        Notiflix.Notify.failure(e.message, {
+            position: 'center-top'
+        });
+    } finally {
+        Notiflix.Loading.remove(1000);
+    }
+}
